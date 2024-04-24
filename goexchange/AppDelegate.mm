070ce8af79fb586dc48b8ff55dc29489b516f3e6
your username <kenrickcanedo@dnamicro.com>
2024-04-25 04:45:16 +0800

    Webview, FaceID and Bridging from Objective C

--- goexchange/AppDelegate.mm
+++ goexchange/AppDelegate.mm
@@ -1,8 +1,10 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <UserNotifications/UserNotifications.h>
 
 @implementation AppDelegate
+NSString *deviceTokenString;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
@@ -11,7 +13,43 @@
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
+  // Request permission to send notifications
+   UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+   [center requestAuthorizationWithOptions:(UNAuthorizationOptionAlert + UNAuthorizationOptionSound + UNAuthorizationOptionBadge)
+                         completionHandler:^(BOOL granted, NSError * _Nullable error) {
+     if (granted) {
+       dispatch_async(dispatch_get_main_queue(), ^{
+         [[UIApplication sharedApplication] registerForRemoteNotifications];
+       });
+     }
+   }];
+  
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+  const unsigned char *dataBuffer = (const unsigned char *)[deviceToken bytes];
+  if (!dataBuffer)
+    return;
+
+  NSUInteger dataLength = [deviceToken length];
+  NSMutableString *hexString  = [NSMutableString stringWithCapacity:(dataLength * 2)];
+  for (int i = 0; i < dataLength; ++i)
+    [hexString appendFormat:@"%02x", (unsigned int)dataBuffer[i]];
+
+  NSLog(@"Device Token: %@", hexString);
+  
+  deviceTokenString = hexString;
+  NSAssert(deviceTokenString != nil, @"deviceTokenString must not be nil");
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+  NSLog(@"Failed to register for remote notifications with error: %@", error);
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+  NSLog(@"Received remote notification: %@", userInfo);
+  completionHandler(UIBackgroundFetchResultNoData);
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
